e8bdf0e8fa8d3c338d6d237be88964f57f7d96c7
ForVic <victor.lakers0@gmail.com>
2021-12-28 16:37:56 -0800

    changed font to 'Gotham'

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask.helpers import url_for
 from flask.templating import render_template
 import json
 from datetime import datetime
 import datetime
+
+from werkzeug.utils import redirect
 f = open('gym_percents.json')
 gym_data = json.load(f)
 
@@ -19,6 +22,10 @@ def index():
     list_of_days = [i.strftime("%m/%d/%y") for i in list_of_days]
     date_time = today.strftime("%d/%m/%y %H:%M:%S")
     return render_template('home.html', context=context, date_time=date_time,list_of_days=list_of_days)
+
+@app.route('/marino')
+def marino():
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
